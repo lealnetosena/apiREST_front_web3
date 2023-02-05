3c0de98065ca9ef1340a7fd77492bad08f87f0e0
José Leal <lealnetosena@gmail.com>
2023-02-05 12:49:41 -0300

    listar informações no front

--- public/javascripts/customers.js
+++ public/javascripts/customers.js
@@ -1,14 +1,39 @@
+/*
+    Promise - fetch()
+            |
+          Pending
+         /       \
+    rejected   fulfilled
+    catch()    then()
+       */
 
+function getAllCostumers() {
+  console.log("=== CUSTOMER ==");
 
-function getAllCostumers(){
-    console.log("=== CUSTOMER ==")
+  const endpoint = `http://localhost:8080/customers`;
 
-    const endpoint = `http://localhost:8080/customers`;
+  const config = {
+    method: "GET",
+  };
 
-    const config = {
-        method: "GET"
-    }
+  fetch(endpoint, config)
+    .then( (res) => res.json())
+        .then(function (customers) {
+        document.querySelector("tbody").innerHTML = 
+            
+            customers
+            .map(function(customers) {
+                return `<tr>
+                    <th scope="row">${customers.id}</th>
+                    <td>${customers.name}</td>
+                    <td>${customers.email}</td>
+                    <td>${customers.cpf}</td>
+                </tr>`
+        
+            }).join("")
 
-    fetch(endpoint,config)
+        })
+        .catch(() => console.log("deu errado"))
+    .catch(() => console.log("deu errado"));
 }
-getAllCostumers()
+getAllCostumers();
